aeac519015ef240d447a7a006a3b3906f106e3ea
DmitriyKharitonov <94749257+DmitriyKharitonov@users.noreply.github.com>
2022-01-29 14:18:01 +0300

    Добавлена jit компиляция

--- inversion_v2.py
+++ inversion_v2.py
@@ -1,15 +1,15 @@
 import matplotlib.pylab as plt
 from PIL import Image
-from numba import njit
+from numba import jit
 
-
+@jit
 def inversion_coordinate(z, x_center, y_center, radius):
 
-    try:
+    if(z.real != x_center or z.imag != y_center):
         new_real = x_center + radius**2*(z.real - x_center)/((z.real - x_center)**2 + (z.imag - y_center)**2)
         new_imag = y_center + radius**2*(z.imag - y_center)/((z.real - x_center)**2 + (z.imag - y_center)**2)
 
-    except ZeroDivisionError:
+    else:
         new_real = x_center
         new_imag = y_center
 
@@ -17,22 +17,23 @@ def inversion_coordinate(z, x_center, y_center, radius):
 
     return new_z
 
+@jit
 def inversion(res, img, wigth, higth, radius, x_center, y_center):
     for x in range(wigth):
         for y in range(higth):
             z = complex(x, y)
             Z = inversion_coordinate(z, x_center, y_center, radius)
             
-            try:
+            if (int(Z.real) <= wigth and int(Z.imag) <= higth):
                 if ((x - x_center)**2 + (y - y_center)**2 > radius**2):
                     res[x,y] = img[int(Z.real), int(Z.imag)]
                     res[int(Z.real), int(Z.imag)] = img[x,y]
-            except:
+            else:
                 continue
     return res
 
 def main():
-    img = plt.imread(r'C:\\Users\\HONOR\\Desktop\\Дима\\Программирвоание\\Инверсия\\Детализирвоанное_фото_v2.jpg')
+    img = plt.imread(r'C:\\Users\\HONOR\\Desktop\\Дима\\Программирвоание\\Инверсия\\Пейзаж.jpg')
     res = img.copy()
     wigth = res.shape[0]
     higth = res.shape[1]
